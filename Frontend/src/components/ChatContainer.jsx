49c23e620aea037b8df5a33d0633c4b4536cc9a0
Mayur <madankarmayur5@gmail.com>
2025-07-15 15:25:09 +0530

    chat-box completed

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useRef } from "react";
 import assets, { messagesDummyData } from "../assets/assets";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  const scrollEnd = useRef(null);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current?.scrollIntoView({ behavior: "smooth" });
+    }
+    inputRef.current?.focus();
+  }, [selectedUser, messagesDummyData]);
+
   return selectedUser ? (
     <div className="h-full relative overflow-scroll backdrop-blur-lg">
       {/* header */}
@@ -29,8 +41,8 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         {messagesDummyData.map((msg, index) => (
           <div
             key={index}
-            className={`flex items-end gap-3 justify-end ${
-              msg.senderId !== "680f5116f10f3cd28382ed02 " && "flex-row-reverse"
+            className={`flex items-end gap-2 justify-end ${
+              msg.senderId !== "680f5116f10f3cd28382ed02" && "flex-row-reverse"
             }`}
           >
             {msg.image ? (
@@ -42,7 +54,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             ) : (
               <p
                 className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
-                  msg.senderId === "680f5116f10f3cd28382ed02"
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
                     ? "rounded-br-none"
                     : "rounded-bl-none"
                 }`}
@@ -60,10 +72,37 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
                 alt=""
                 className="w-7 rounded-full"
               />
-              <p className="text-gray-500">{msg.createdAt}</p>
+              <p className="text-gray-500">
+                {formatMessageTime(msg.createdAt)}
+              </p>
             </div>
           </div>
         ))}
+        <div ref={scrollEnd}></div>
+      </div>
+      {/* Bottom area  */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Send the message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+          />
+          <input type="file" id="image" accept="image/png,image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              alt="gallary-icon"
+              className="w-5 mr-2 cursor-pointer"
+            />
+          </label>
+        </div>
+        <img
+          src={assets.send_button}
+          alt="send-button"
+          className="w-7 cursor-pointer"
+        />
       </div>
     </div>
   ) : (
